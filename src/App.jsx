1a2dd051450da689e3a355673fa57eac045a41e4
Muhammad Selim <muhamadselim70@gmail.com>
2023-08-22 15:09:29 +0300

    BrainUp setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,47 +1,11 @@
-import React, { useState } from "react";
+import React from "react";
 import { createRoot } from "react-dom/client";
-import "./index.css";
+import "./assets/styles/index.css";
 
 const App = () => {
-  const [counter, setCounter] = useState(0);
   return (
-    <div className="relative w-full h-screen bg-slate-800 flex flex-col justify-center items-center gap-4 text-gray-200">
-      <h1 className="w-full text-center text-4xl font-medium">
-        React + Tailwind
-      </h1>
-      <div className="flex">
-        <img
-          src="https://www.svgrepo.com/show/452092/react.svg"
-          alt="React Logo"
-          className="w-40 h-40"
-        />
-        <img
-          src="https://www.svgrepo.com/show/374118/tailwind.svg"
-          alt="Tailwind Logo"
-          className="w-40 h-40"
-        />
-      </div>
-      <div className="flex flex-col items-center gap-4">
-        <span className="font-bold text-2xl">{counter}</span>
-        <button
-          className="px-4 py-2 border-2 rounded-md bg-cyan-900 text-xl font-medium"
-          onClick={() => {
-            setCounter(counter + 1);
-          }}
-        >
-          counter
-        </button>
-      </div>
-      <span className="absolute left-1/2 -translate-x-1/2 bottom-0 font-mono">
-        created by{" "}
-        <a
-          href="https://github.com/Mu-selim"
-          target="_blanck"
-          className="text-cyan-200"
-        >
-          Muhammad Selim
-        </a>
-      </span>
+    <div className="relative w-full h-screen bg-secondary flex flex-col justify-center items-center gap-4 text-gray-200">
+      <h1 className="w-full text-center text-4xl font-medium">BrainUp</h1>
     </div>
   );
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,34 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    screens: {
+      xs: "380px",
+      // => @media (min-width: 380px) { ... }
+      sm: "640px",
+      // => @media (min-width: 640px) { ... }
+      md: "768px",
+      // => @media (min-width: 768px) { ... }
+      lg: "1024px",
+      // => @media (min-width: 1024px) { ... }
+      xl: "1280px",
+      // => @media (min-width: 1280px) { ... }
+      "2xl": "1536px",
+      // => @media (min-width: 1536px) { ... }
+    },
+    extend: {
+      colors: {
+        primary: "#2d2478",
+        "primary-light": "#5357a9",
+        secondary: "#efbc54",
+        "secondary-light": "#f1e6d3",
+      },
+      fontFamily: {
+        inter: ["Inter", "sans-serif"],
+        nunito: ["Nunito", "sans-serif"],
+        arabic: ["CooperArabic", "sans-serif"],
+      },
+    },
   },
   plugins: [],
-}
-
+};
