f0e98252e03c13ae6aa3d962df332ec47af68509
Muhammad Selim <muhamadselim70@gmail.com>
2023-08-24 12:04:33 +0300

    adding routing

--- src/App.jsx
+++ src/App.jsx
@@ -2,25 +2,13 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 
 // router
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { RouterComponent } from "./router/Router";
 
 // assets
 import "./assets/styles/index.css";
 
-// pages
-import { HomePage } from "./pages/home/Index";
-import { Navbar } from "./components/Navbar";
-
 const App = () => {
-  return (
-    <Router>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="*" element={<h1>404</h1>} />
-      </Routes>
-    </Router>
-  );
+  return <RouterComponent />;
 };
 
 const container = document.getElementById("root");
