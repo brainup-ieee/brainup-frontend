0b8e4776b47d3e5fd79095e11e45b58e6fece5ed
Muhammad Selim <muhamadselim70@gmail.com>
2023-09-10 16:09:36 +0300

    enhance home page

--- src/pages/home/Index.jsx
+++ src/pages/home/Index.jsx
@@ -6,12 +6,12 @@ import landingImage from "../../assets/images/landing-original.png";
 
 const LandingSection = () => {
   return (
-    <section className="w-full h-[calc(100vh-6rem)] max-h-[40rem] p-8 bg-secondary rounded-3xl flex justify-between items-center">
+    <section className="w-full h-[calc(100vh-7rem)] max-h-[40rem] p-8 bg-secondary rounded-3xl flex justify-between items-center">
       <div className="flex flex-col justify-center gap-4 basis-1/2">
-        <h2 className=" font-nunito font-extrabold text-5xl leading-tight	">
+        <h2 className="font-nunito font-extrabold text-5xl leading-tight	">
           Empower Your Classroom with BrainUp
         </h2>
-        <p className=" font-inter mb-4">
+        <p className="font-inter mb-4">
           Revolutionizing the Landscape of Education by Introducing Cutting-Edge
           and Innovative Learning Approaches That Transform the Way Students
           Learn and Teachers Instruct.
